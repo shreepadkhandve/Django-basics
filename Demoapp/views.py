2896628b15584f6fe4e870259d5e9e256b9ee056
Shreepad <shreepad.khandve@sitpune.edu.in>
2019-07-18 12:05:04 +0530

    changes in views.py

--- Demoapp/views.py
+++ Demoapp/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def homepage(request):
-	print('hello')
-	print('hello')
-	print('hello')
-	print('hello')
-	print('hello')
-	print('hello')
 	return render(request,'homepage.html')
